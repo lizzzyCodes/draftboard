f7a8b7f8e50e11dc732e670a2bbb941e04d691ff
lizzzyCodes <147952557+lizzzyCodes@users.noreply.github.com>
2026-07-23 14:43:48 -0700

    adding fonts and creating skeletons for card component

--- draftboard/app/page.tsx
+++ draftboard/app/page.tsx
@@ -1,65 +1,110 @@
-import Image from "next/image";
+// it will disply the stock image and everything else. 
+// for now hardcoded  hardcoding a random player and adding an image to see if ti works before spending so much time on it
 
-export default function Home() {
+import React from 'react';
+import Image from 'next/image'
+import "@fontsource/oswald";
+import "@fontsource/passion-one";
+import "@fontsource/luckiest-guy";
+import { Stars } from "@/app/components/Stars"
+
+
+export default function Card() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+
+      <div style={{ backgroundColor: "#002D62" }}>
+        <h1 style={{ color: "#FFFFFF", fontSize: 200 }} > Oliver Giroud</h1>
+        <div>
+          <div>
+            <p>honors</p>
+          </div>
+        </div>
+      </div>
+      <div>
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src="/player-images/lukas.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+        <section style={{ backgroundColor: "#240050", width: '200px', height: '500px' }}>
+          <section className="flex justify-center items-baseline gap-2">
+            <div className="flex gap-2">{Stars}</div>
+
+            <h1
+              className="flex items-center text-black"
+              style={{ fontFamily: "Luckiest Guy" }}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+              <p>NBA HO</p>
+              <Image
+                src="/player-images/basketball.svg"
+                width={15}
+                height={15}
+                style={{ paddingBottom: '5px' }}
+                alt="Basketball"
+              />
+              <p>PS</p>
+            </h1>
+            <div className="flex gap-2">{Stars}</div>
+          </section>
+
+
+          <div style={{ borderRadius: "50%", width: '150px', height: '150px', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center', borderColor: '#EAC77C', borderStyle: 'solid', borderWidth: '2px' }}>
+            <div style={{ width: "100%", height: "100%", objectFit: "cover" }}>
+              <Image
+                src="/player-images/lakers.jpg"
+                width={500}
+                height={500}
+                alt="Picture of the author"
+              />
+            </div>
+          </div>
+          <section>
+            <p style={{ fontFamily: 'Oswald', color: "#FFFFFF" }}>LeBRON JAMES</p>
+            <p style={{ fontFamily: 'Passion One', color: "#FFFFFF" }}>LAKERS</p>
+          </section>
+        </section >
+      </div >
+    </>
+
   );
 }
+
+// 2. Build the functional component
+/*export default function MyComponent() {
+  return (
+    <>
+      <div>
+        <p> MINNESOTA NORTHSTARS</p>
+        <h1> CALEB THORNE</h1>
+        <p>"THE HAWK"</p>
+        <p>GUARD</p>
+        <p>6.4</p>
+        <p>WINGSPAN 6'8"</p>
+      </div>
+      <div>
+        <h1 style={{ color: "#552583", fontSize: 200 }} > CLIPPERS</h1>
+        <h1 style={{ color: "#552583", fontSize: 200 }}> #13</h1>
+      </div>
+
+      <div style={{ backgroundColor: "#002D62" }}>
+        <h1 style={{ color: "#FFFFFF", fontSize: 200 }} > Oliver Giroud</h1>
+        <div style={{ backgroundColor: lighten("#002D62", 0.15) }}>
+          <div>
+            <p>honors</p>
+          </div>
+        </div>
+        <Image
+          src="/player-images/OklahomaCity.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </div>
+    </>
+
+  );
+} */
+
+Card()
