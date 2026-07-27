c175c20b098612782824114a69a7c12ecc241b2d
lizzzyCodes <147952557+lizzzyCodes@users.noreply.github.com>
2026-07-26 19:27:48 -0700

    addinf skeleton code for table

--- draftboard/app/components/PlayerCard/CardBack.tsx
+++ draftboard/app/components/PlayerCard/CardBack.tsx
@@ -4,25 +4,26 @@ import MetricContainer from "./MetricContainer";
 import PlayerInfo from "./PlayerInfo";
 import PlayerBio from "./PlayerBio";
 import ScoutReportsSection from "./ScoutReports";
+import CardFooter from "./CardFooter";
 
 interface CardBackProps {
-  backgroundColor?: string;
-  borderColor?: string;
-  teamAbbreviation: keyof typeof NBAIcons;
-  playerName?: string;
+    backgroundColor?: string;
+    borderColor?: string;
+    teamAbbreviation: keyof typeof NBAIcons;
+    playerName?: string;
 }
 
 export default function CardBack({
-  backgroundColor,
-  borderColor,
-  teamAbbreviation,
-  playerName,
+    backgroundColor,
+    borderColor,
+    teamAbbreviation,
+    playerName,
 }: CardBackProps) {
-  const TeamIcon = NBAIcons[teamAbbreviation]; // cant use ? or else its going to cmplin keep in mind
+    const TeamIcon = NBAIcons[teamAbbreviation]; // cant use ? or else its going to cmplin keep in mind
 
-  return (
-    <section
-      className="
+    return (
+        <section
+            className="
         rounded-tl-[20px]
         rounded-tr-[20px]
         rounded-bl-[20px]
@@ -31,12 +32,12 @@ export default function CardBack({
         overflow-hidden
         relative
       "
-      style={{
-        backgroundColor,
-      }}
-    >
-      <div
-        className="
+            style={{
+                backgroundColor,
+            }}
+        >
+            <div
+                className="
                 h-full
                 flex
                 flex-col
@@ -45,21 +46,22 @@ export default function CardBack({
             rounded-tr-[20px]
             rounded-bl-[20px]
             rounded-br-[20px]"
-        style={{ borderColor: borderColor }}
-      >
-        <BackButton />
-        <div className="size-25">
-          <TeamIcon />
-        </div>
-        <h3 style={{ color: borderColor }}>{playerName} </h3>
-        <PlayerInfo color={borderColor} />
-        <MetricContainer color={borderColor} />
-        <PlayerBio
-          color={borderColor}
-          bio="Four-time champion and the league's all-time leading scorer. Still running the offense at an elite level deep into his twenties year."
-        />
-        <ScoutReportsSection color={borderColor} />
-      </div>
-    </section>
-  );
+                style={{ borderColor: borderColor }}
+            >
+                <BackButton />
+                <div className="size-25">
+                    <TeamIcon />
+                </div>
+                <h3 style={{ color: borderColor }}>{playerName} </h3>
+                <PlayerInfo color={borderColor} />
+                <MetricContainer color={borderColor} />
+                <PlayerBio
+                    color={borderColor}
+                    bio="Four-time champion and the league's all-time leading scorer. Still running the offense at an elite level deep into his twenties year."
+                />
+                <ScoutReportsSection color={borderColor} />
+                <CardFooter />
+            </div>
+        </section>
+    );
 }
---
+++ draftboard/app/components/PlayerCard/CardFooter.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function CardFooter() {
+  return (
+    <footer className=" text-white">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col sm:flex-row items-center justify-between text-sm">
+        <Image src="/player-images/NBAPNG.png" alt="NBA Logo" width={20} height={20} />
+        <p>Copr. © 2026 NBA Properties, Inc. </p>
+        <p>The Official NFT Basketball Card </p>
+        <div className="flex space-x-4 mt-4 sm:mt-0">
+          <p>© 2026 SKYBOX INTERNATIONAL </p>
+          <p>Created in USA</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
